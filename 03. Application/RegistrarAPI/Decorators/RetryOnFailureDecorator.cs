177bb0dcd34fb7da8c9be45ddc6191093c46ac0a
Ashwin Purushotaman <apurushotaman@xactlycorp.com>
2022-01-10 14:00:41 +0000

    add retry decorator

---
+++ 03. Application/RegistrarAPI/Decorators/RetryOnFailureDecorator.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Commands;
+
+namespace RegistrarAPI.Decorators
+{
+    public class RetryOnFailureDecorator<TCommand> : ICommandHandler<TCommand>
+        where TCommand : ICommand
+    {
+        private readonly ICommandHandler<TCommand> commandHandler;
+
+        public RetryOnFailureDecorator(ICommandHandler<TCommand> commandHandler)
+        {
+            this.commandHandler = commandHandler;
+        }
+        public async Task HandleAsync(TCommand command)
+        {
+            for (int i = 0;i < 3; i++)
+            {
+                try
+                {
+                    await commandHandler.HandleAsync(command);
+                    break;
+                }
+                catch
+                {
+                    continue;
+                }
+            }
+        }
+    }
+}
